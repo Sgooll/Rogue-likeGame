4fb241fed9d3352ce5c0c9931d56b7c5c757b92c
Sgooll <75836663+Sgooll@users.noreply.github.com>
2021-03-01 22:14:04 +0300

    Конечный алгоритм генерации комнат
    Двери спавнятся в тех местах, где нет соседей комнат

--- PlaceRoom.cpp
+++ PlaceRoom.cpp
@@ -12,88 +12,270 @@ APlaceRoom::APlaceRoom()
 
 }
 
+void APlaceRoom::PlaceXplusDoor()
+{
+	bool spn = false;
+	FVector Location = GetActorLocation();
+	FRotator Rotation = GetActorRotation();
+	for (int j = 0; j < 3; j++)
+	{
+		if (Location.X + 2420  == NeighboursX[j])
+		{
+			spn = true;
+			break;
+		}
+	}
+	if (spn != true)
+	{
+		Location.X += 1260 ;
+		Rotation.Yaw += 90;
+		SetActorLocation(Location);
+		SetActorRotation(Rotation);
+		GetWorld()->SpawnActor<AActor>(Door, GetActorLocation(), GetActorRotation());
+		Location.X -= 1260 ;
+		Rotation.Yaw -= 90;
+		SetActorLocation(Location);
+		SetActorRotation(Rotation);
+	}
+}
+
+void APlaceRoom::PlaceYplusDoor()
+{
+	bool spn = false;
+	FVector Location = GetActorLocation();
+	FRotator Rotation = GetActorRotation();
+	for (int j = 0; j < 3; j++)
+	{
+		if (Location.Y + 2420  == NeighboursY[j])
+		{
+			spn = true;
+			break;
+		}
+	}
+	if (spn != true)
+	{
+		Location.Y += 1200 ;
+		//Rotation.Yaw += 90;
+		SetActorLocation(Location);
+		//SetActorRotation(Rotation);
+		GetWorld()->SpawnActor<AActor>(Door, GetActorLocation(), GetActorRotation());
+		Location.Y -= 1200 ;
+		//Rotation.Yaw -= 90;
+		SetActorLocation(Location);
+		//SetActorRotation(Rotation);
+	}
+}
+void APlaceRoom::PlaceXminusDoor()
+{
+	bool spn = false;
+	FVector Location = GetActorLocation();
+	FRotator Rotation = GetActorRotation();
+	for (int j = 0; j < 3; j++)
+	{
+		if (Location.X - 2420  == NeighboursX[j])
+		{
+			spn = true;
+			break;
+		}
+	}
+	if (spn != true)
+	{
+		Location.X -= 1200 ;
+		Rotation.Yaw += 90;
+		SetActorLocation(Location);
+		SetActorRotation(Rotation);
+		GetWorld()->SpawnActor<AActor>(Door, GetActorLocation(), GetActorRotation());
+		Location.X += 1200 ;
+		Rotation.Yaw -= 90;
+		SetActorLocation(Location);
+		SetActorRotation(Rotation);
+	}
+}
+void APlaceRoom::PlaceYminusDoor()
+{
+	bool spn = false;
+	FVector Location = GetActorLocation();
+	FRotator Rotation = GetActorRotation();
+	for (int j = 0; j < 3; j++)
+	{
+		if (Location.Y - 2420  == NeighboursY[j])
+		{
+			spn = true;
+			break;
+		}
+	}
+	if (spn != true)
+	{
+		Location.Y -= 1250 ;
+		//Rotation.Yaw += 90;
+		SetActorLocation(Location);
+		//SetActorRotation(Rotation);
+		GetWorld()->SpawnActor<AActor>(Door, GetActorLocation(), GetActorRotation());
+		Location.Y += 1250 ;
+		//Rotation.Yaw -= 90;
+		SetActorLocation(Location);
+		//SetActorRotation(Rotation);
+	}
+}
+
 // Called when the game starts or when spawned
 void APlaceRoom::BeginPlay()
 {
 	Super::BeginPlay();
 	srand(time(0));
 	GetWorld()->SpawnActor<AActor>(ActorToSpawn, GetActorLocation(), GetActorRotation());
-		for (int i = 0; i < 10; i++) {
+	SpawnedRoomsX[0] = GetActorLocation().X;
+	SpawnedRoomsY[0] = GetActorLocation().Y;
+		for (int i = 1; i < 10; i++) {
 			bool x = false, y = false, mx = false, my = false;
-			const FVector Location = GetActorLocation();
+			//FVector Location = GetActorLocation();
 			const FRotator Rotation = GetActorRotation();
 			FVector NewLocation = GetActorLocation();
-			int ConX = rand() % 4;
-			int ConY = rand() % 4;
-			for (int loc = 0; loc < 10; loc++)
+			int ConX = rand() % 3;
+			int ConY = rand() % 3;
+			for (int loc = 0; loc < 20; loc++)
 			{
-				if (NewLocation.Y + 2425.0f == SpawnedRoomsY[loc])
+				if (NewLocation.Y + 2420  == SpawnedRoomsY[loc])
+				{
 					y = true;
-				if (NewLocation.Y - 2425.0f == SpawnedRoomsY[loc])
+				}
+				if (NewLocation.Y - 2420  == SpawnedRoomsY[loc])
+				{
 					my = true;
-				if (NewLocation.X + 2425.0f == SpawnedRoomsX[loc])
+				}
+				if (NewLocation.X + 2420  == SpawnedRoomsX[loc])
+				{
 					x = true;
-				if (NewLocation.X - 2425.0f == SpawnedRoomsX[loc])
+				}
+				if (NewLocation.X - 2420  == SpawnedRoomsX[loc])
+				{
 					mx = true;
+				}
 			}
 					if ((ConX == 0) && (x == false))
 					{
-						NewLocation.X += 2425.0f;
+						PlaceYminusDoor();
+						PlaceXminusDoor();
+						PlaceYplusDoor();
+						NewLocation.X += 2420 ;
 						SetActorLocation(NewLocation);
 						GetWorld()->SpawnActor<AActor>(ActorToSpawn, NewLocation, Rotation);
+						SpawnedRoomsX[i] = NewLocation.X;
+						NeighboursX[1] = NewLocation.X - 2420;
+						NeighboursY[0] = 0;
+						NeighboursY[1] = 0;
 						x = true;
 					}
 					if ((ConX == 1) && (mx == false))
 					{
-						NewLocation.X -= 2425.0f;
+						PlaceYminusDoor();
+						PlaceYplusDoor();
+						PlaceXplusDoor();
+						NewLocation.X -= 2420 ;
 						SetActorLocation(NewLocation);
 						GetWorld()->SpawnActor<AActor>(ActorToSpawn, NewLocation, Rotation);
+						SpawnedRoomsX[i] = NewLocation.X;
+						NeighboursX[0] = NewLocation.X + 2420;
+						NeighboursY[0] = 0;
+						NeighboursY[1] = 0;
 						mx = true;
 					}
 					if (ConX == 2 && (x == false) && (mx == false))
 					{
-						NewLocation.X += 2425.0f;
+						PlaceYminusDoor();
+						PlaceYplusDoor();
+						NewLocation.X += 2420 ;
 						SetActorLocation(NewLocation);
 						GetWorld()->SpawnActor<AActor>(ActorToSpawn, NewLocation, Rotation);
-						NewLocation.X -= 4850.0f;
-						SetActorLocation(NewLocation);
-						GetWorld()->SpawnActor<AActor>(ActorToSpawn, NewLocation, Rotation);
+						SpawnedRoomsX[i] = NewLocation.X;
+						NeighboursY[0] = 0;
+						NeighboursY[1] = 0;
+						if (i != 9)
+						{
+							i++;
+							PlaceYminusDoor();
+							PlaceYplusDoor();
+							PlaceXplusDoor();
+							NewLocation.X -= 4840 ;
+							SetActorLocation(NewLocation);
+							GetWorld()->SpawnActor<AActor>(ActorToSpawn, NewLocation, Rotation);
+							SpawnedRoomsX[i] = NewLocation.X;
+							NeighboursX[0] = NewLocation.X + 2420;
+							NeighboursY[0] = 0;
+							NeighboursY[1] = 0;
+							mx = true;
+						}
 						x = true;
-						mx = true;
 					}
-					if (ConX == 3) continue;
 					if (ConY == 0 && y == false)
 					{
-						NewLocation.Y += 2425.0f;
+						PlaceYminusDoor();
+						PlaceXminusDoor();
+						PlaceXplusDoor();
+						NewLocation.Y += 2420 ;
 						SetActorLocation(NewLocation);
 						GetWorld()->SpawnActor<AActor>(ActorToSpawn, NewLocation, Rotation);
+						SpawnedRoomsY[i] = NewLocation.Y;
+						NeighboursY[1] = NewLocation.Y - 2420;
+						NeighboursX[0] = 0;
+						NeighboursX[1] = 0;
 						y = true;
 					}
 					if (ConY == 1 && my == false)
 					{
-						NewLocation.Y -= 2425.0f;
+						PlaceXminusDoor();
+						PlaceYplusDoor();
+						PlaceXplusDoor();
+						NewLocation.Y -= 2420 ;
 						SetActorLocation(NewLocation);
 						GetWorld()->SpawnActor<AActor>(ActorToSpawn, NewLocation, Rotation);
+						SpawnedRoomsY[i] = NewLocation.Y;
+						NeighboursY[0] = NewLocation.Y + 2420;
+						NeighboursX[0] = 0;
+						NeighboursX[1] = 0;
 						my = true;
 					}
 					if (ConY == 2  && y == false && my == false)
 					{
-						NewLocation.Y += 2425.0f;
+						PlaceXminusDoor();
+						PlaceXplusDoor();
+						NewLocation.Y += 2420 ;
 						SetActorLocation(NewLocation);
 						GetWorld()->SpawnActor<AActor>(ActorToSpawn, NewLocation, Rotation);
-						NewLocation.Y -= 4850.0f;
-						SetActorLocation(NewLocation);
-						GetWorld()->SpawnActor<AActor>(ActorToSpawn, NewLocation, Rotation);
+						SpawnedRoomsY[i] = NewLocation.Y;
+						NeighboursX[0] = 0;
+						NeighboursX[1] = 0;
+						if (i != 9)
+						{
+							i++;
+							PlaceXminusDoor();
+							PlaceYplusDoor();
+							PlaceXplusDoor();
+							NewLocation.Y -= 4840;
+							SetActorLocation(NewLocation);
+							GetWorld()->SpawnActor<AActor>(ActorToSpawn, NewLocation, Rotation);
+							SpawnedRoomsY[i] = NewLocation.Y;
+							NeighboursY[0] = NewLocation.Y + 2420;
+							NeighboursX[0] = 0;
+							NeighboursX[1] = 0;
+							my = true;
+						}
 						y = true;
-						my = true;
 					}
-					if (ConY == 3) continue;
-					SpawnedRoomsY[i] = NewLocation.Y;
-					SpawnedRoomsX[i] = NewLocation.X;
-				
+
 
 		}
-
+		if (NeighboursY[9] + 2420 != 0)
+		{
+			PlaceYminusDoor();
+			PlaceXminusDoor();
+			PlaceXplusDoor();
+		}
+		if (NeighboursY[9] - 2420 != 0) {
+			PlaceXminusDoor();
+			PlaceYplusDoor();
+			PlaceXplusDoor();
+		}
 }
 
 // Called every frame
@@ -102,4 +284,6 @@ void APlaceRoom::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
+
+
 
